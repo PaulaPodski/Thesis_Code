f1a43e4c9f0d362c474588a101e4121836ea0476
Paula Podski <paulapodski29@gmail.com>
2026-08-17 11:18:19 +0100

    Added the 12L DMA script with full DMA pipeline as it was missing the .rds object from normalised step

--- scripts/Results_Section_4.2_Genome_Wide/04_run_dma_12L.R
+++ scripts/Results_Section_4.2_Genome_Wide/04_run_dma_12L.R
@@ -2,6 +2,18 @@
 
 library(methylKit)
 
+#Firstly we will load the normalised methylKit object:
+
+normalized.myobj <- readRDS("normalized.myobj.rds")
+
+#Then I create and save the 12L united rds object:
+
+meth_12 <- unite(normalized.myobj, destrand = FALSE, min.per.group = 12L)
+
+saveRDS(meth_12, "meth_min12.rds")
+
+#Finally,I run the DMA on the united 12L rds object:
+
 meth_12 <- readRDS("meth_min12.rds")
 
 myDiff_12 <- calculateDiffMeth(meth_12)
